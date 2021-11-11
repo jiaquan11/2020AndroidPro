9c8b3af0f8e4eee8747f139f9dcf72667fcfc5e3
jiaquanxia <913882743@qq.com>
2021-11-11 11:50:52 +0800

    *XiangXueClass-MyButton项目： 实现Button的点击事件

--- XiangXueClass/MyButton/app/src/main/java/com/example/mybutton/MainActivity.java
+++ XiangXueClass/MyButton/app/src/main/java/com/example/mybutton/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.mybutton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btn);
+        //点击事件
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e(TAG, "onClick: ");
+            }
+        });
+
+        //长按事件
+        btn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Log.e(TAG, "onLongClick: ");
+                return false;
+            }
+        });
+
+        //触摸事件
+        btn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.e(TAG, "onTouch: " + event.getAction());
+                return false;
+            }
+        });
     }
 }
