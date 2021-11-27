617ca7bf00dc2992863ae65ca76771554c345730
jiaquan11 <913882743@qq.com>
2021-11-27 12:03:17 +0800

    *XiangXueClass-XJQ_fragmentmanagement项目：fragment的生命周期

--- XiangXueClass/XJQ_FragmentManagers/app/src/main/java/com/example/xjq_fragmentmanagers/BlankFragment1.java
+++ XiangXueClass/XJQ_FragmentManagers/app/src/main/java/com/example/xjq_fragmentmanagers/BlankFragment1.java
@@ -1,7 +1,10 @@
 package com.example.xjq_fragmentmanagers;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -25,6 +28,12 @@ public class BlankFragment1 extends Fragment {
     }
 
     @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Log.i(TAG, "onAttach: ");
+    }
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -45,17 +54,69 @@ public class BlankFragment1 extends Fragment {
             @Override
             public void onClick(View v) {
 //                fragmentCallback.sendMsgToActivity("hello, I'm from fragment");
-                
+
                 String msg = fragmentCallback.getMsgFromActivity("null");
                 Toast.makeText(BlankFragment1.this.getContext(), msg, Toast.LENGTH_SHORT).show();
             }
         });
+
+        Log.i(TAG, "onCreateView: ");
         return rootview;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Log.i(TAG, "onActivityCreated: ");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        Log.i(TAG, "onStart: ");
     }
 
     @Override
     public void onResume() {
         super.onResume();
 
+        Log.i(TAG, "onResume: ");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        Log.i(TAG, "onPause: ");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        Log.i(TAG, "onStop: ");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        Log.i(TAG, "onDestroyView: ");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        Log.i(TAG, "onDestroy: ");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+        Log.i(TAG, "onDetach: ");
     }
 }
--- XiangXueClass/XJQ_FragmentManagers/app/src/main/java/com/example/xjq_fragmentmanagers/MainActivity.java
+++ XiangXueClass/XJQ_FragmentManagers/app/src/main/java/com/example/xjq_fragmentmanagers/MainActivity.java
@@ -56,7 +56,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.framelayout, fragment);
-        transaction.addToBackStack(null);
+//        transaction.addToBackStack(null);
         transaction.commit();
     }
 }
